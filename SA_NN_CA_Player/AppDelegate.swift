50b1e9fed25a5ac8f226f68e52b256d9fb8260b0
DmitriyLeaf <dmitriy.kruglov.work@gmail.com>
2019-11-17 23:16:51 +0200

    Added background playing

--- SA_NN_CA_Player/AppDelegate.swift
+++ SA_NN_CA_Player/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let session = AVAudioSession.sharedInstance()
+        do{
+            try session.setActive(true)
+            try session.setCategory(.playback, mode: .default,  options: .defaultToSpeaker)
+        } catch{
+            print(error.localizedDescription)
+        }
         return true
     }
 
